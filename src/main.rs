042d188aa0776fc08cae782ca8b98c49d3910d98
tim <tmsteem82@gmail.com>
2021-09-08 16:12:35 +0200

    read from YAML file and print values

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use serde::{Deserialize, Serialize};
+use serde_yaml::{self};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Config {
+    update_frequency_sec: u32,
+    num_threads: u32,
+    data_sources: Vec<String>,
+}
 fn main() {
-    println!("Hello, world!");
+    let f = std::fs::File::open("config.yml").expect("Could not open file.");
+    let scrape_config: Config = serde_yaml::from_reader(f).expect("Could not read values.");
+
+    println!("{:?}", scrape_config);
+
+    println!(
+        "update_frequency_sec: {}",
+        scrape_config.update_frequency_sec
+    );
+
+    for data_source in scrape_config.data_sources.iter() {
+        println!("{}", data_source);
+    }
 }
