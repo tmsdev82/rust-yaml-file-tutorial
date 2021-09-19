bae061a899ecda1f3fb4d96f80fdf47afc81a2a0
tim <tmsteem82@gmail.com>
2021-09-19 08:44:51 +0200

    add example for array of objects in YAML

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,17 @@ use serde::{Deserialize, Serialize};
 use serde_yaml::{self};
 
 #[derive(Debug, Serialize, Deserialize)]
+struct DataSource {
+    name: String,
+    url: String,
+    source_type: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
 struct Config {
     update_frequency_sec: u32,
     num_threads: u32,
-    data_sources: Vec<String>,
+    data_sources: Vec<DataSource>,
 }
 fn main() {
     let f = std::fs::File::open("config.yml").expect("Could not open file.");
@@ -19,17 +26,24 @@ fn main() {
     );
 
     for data_source in scrape_config.data_sources.iter() {
-        println!("{}", data_source);
+        println!(
+            "name: {}, type: {}, url {}",
+            data_source.name, data_source.source_type, data_source.url
+        );
     }
 
     scrape_config.num_threads = 2;
 
-    scrape_config
-        .data_sources
-        .push("www.nytimes.com".to_string());
-    scrape_config
-        .data_sources
-        .push("news.yahoo.com".to_string());
+    scrape_config.data_sources.push(DataSource {
+        name: "NYTimes".to_string(),
+        url: "www.nytimes.com".to_string(),
+        source_type: "news".to_string(),
+    });
+    scrape_config.data_sources.push(DataSource {
+        name: "Yahoo News".to_string(),
+        url: "news.yahoo.com".to_string(),
+        source_type: "news".to_string(),
+    });
 
     let f = std::fs::OpenOptions::new()
         .write(true)
